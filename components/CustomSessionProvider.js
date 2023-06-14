cf16008ee4b4e9f12a1141d0fc9a71f63823232d
nirnay <nirnaykorde@gmail.com>
2023-06-14 15:07:23 +0530

    Temporary Setup

---
+++ components/CustomSessionProvider.js
@@ -0,0 +1,11 @@
+'use client';
+
+import { SessionProvider } from "next-auth/react";
+
+const CustomSessionProvider = ({ children, session }) => (
+    <SessionProvider session={session}>
+        {children}
+    </SessionProvider>
+)
+
+export default CustomSessionProvider;
