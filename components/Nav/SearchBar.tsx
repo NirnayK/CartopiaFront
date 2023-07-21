470e2884cc4fead317b050e70de4531ae59502fe
nirnay <nirnaykorde@gmail.com>
2023-07-21 21:38:34 +0530

    Search Bar made with only console log

--- components/Nav/SearchBar.tsx
+++ components/Nav/SearchBar.tsx
@@ -1,5 +1,31 @@
 "use client";
-import { useState, ChangeEvent, FormEvent } from "react";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Search } from "lucide-react";
+
+const FormSchema = z.object({
+  name: z.string().min(3),
+  categoryId: z.string().nonempty(),
+});
 
 interface Category {
   _id: string;
@@ -11,66 +37,65 @@ interface SearchbarProps {
 }
 
 const Searchbar: React.FC<SearchbarProps> = ({ categories }) => {
-  const [category, setCategory] = useState<string>("all");
-  const [search, setSearch] = useState<string>("");
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
 
-  const handleSumbit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log(category, search);
-  };
-
-  const handleCategoryChange = (e: ChangeEvent<HTMLSelectElement>) => {
-    setCategory(e.target.value);
-  };
-
-  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-  };
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(data);
+  }
 
   return (
-    <form className="flex items-center" onSubmit={handleSumbit}>
-      <select
-        className="h-10 w-1/6 text-slate-600 rounded-l-md border-2 border-r-0 border-gray-200"
-        id="category"
-        name="category"
-        value={category}
-        onChange={handleCategoryChange}
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-min flex flex-row items-center"
       >
-        <option value="all">All</option>
-        {categories.map((category) => (
-          <option key={category._id} value={category._id}>
-            {category.name}
-          </option>
-        ))}
-      </select>
-      <div className="relative">
-        <input
-          className="h-10 rounded-r-full text-slate-600 border-2 border-l-0 border-gray-200 p-2 pr-9"
-          type="text"
-          id="name"
-          placeholder="Search"
-          value={search}
-          onChange={handleSearchChange}
+        <FormField
+          control={form.control}
+          name="categoryId"
+          render={({ field }) => (
+            <FormItem>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger className="w-[180px] rounded-r-none">
+                    <SelectValue defaultValue="All" placeholder="All" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="m@example.com">m@example.com</SelectItem>
+                  <SelectItem value="m@google.com">m@google.com</SelectItem>
+                  <SelectItem value="m@support.com">m@support.com</SelectItem>
+                </SelectContent>
+              </Select>
+            </FormItem>
+          )}
         />
-        <button
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  className="w-auto rounded-none "
+                  placeholder="Enter product Name"
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <Button
+          className="rounded-l-none rounded-r-full"
+          variant="outline"
+          size="icon"
           type="submit"
-          className="absolute top-1/2 right-3 transform -translate-y-1/2 bg-transparent border-none outline-none cursor-pointer"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10.5 3.75a6.75 6.75 0 100 13.5 6.75 6.75 0 000-13.5zM2.25 10.5a8.25 8.25 0 1114.59 5.28l4.69 4.69a.75.75 0 11-1.06 1.06l-4.69-4.69A8.25 8.25 0 012.25 10.5z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </button>
-      </div>
-    </form>
+          <Search />
+        </Button>
+      </form>
+    </Form>
   );
 };
 
